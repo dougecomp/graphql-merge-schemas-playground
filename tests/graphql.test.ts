70d508fb2fcc38ab3b851beaa54dbb4a305c1f06
Douglas Silva <douglaseusilva@gmail.com>
2024-04-01 19:32:10 -0300

    feat: validação dos endoints com testes no jest

---
+++ tests/graphql.test.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata'
+
+import request from 'supertest'
+
+import { YogaServerInstance } from "graphql-yoga";
+import { makeApp } from '../src/app';
+
+describe('GraphQL Merge Schemas', () => {
+  let app: YogaServerInstance<{},{}>
+
+  beforeAll(async () => {
+    app = await makeApp()
+  })
+
+  it('should hit removeRecipe from Typegraphql schema generation', async () => {
+    const removeRecipeQuery = `
+      mutation {
+        removeRecipe(id: "1")
+      }
+    `
+    const res = await request(app)
+    .post('/graphql')
+    .send({query: removeRecipeQuery})
+
+    expect(res.body.data.removeRecipe).toBeDefined()
+  })
+
+  it('should hit removeUser from manual schema generation', async () => {
+    const removeRecipeQuery = `
+      mutation {
+        removeUser(id: "1")
+      }
+    `
+    const res = await request(app)
+    .post('/graphql')
+    .send({query: removeRecipeQuery})
+
+    expect(res.body.data.removeUser).toBeDefined()
+  })
+})
